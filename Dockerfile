ef717d7356c00beb69cf1d7eb1ad1a380cd4dfbb
Glaucia Lauton <glaucia.brito@maps.com.br>
2024-03-31 19:43:30 -0300

    fix: adding the correct version of mongoengine

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /app
 
 COPY requirements.txt .
 
+RUN apk add git
+RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
 COPY app.py .
--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
+from mongoengine.fields import StringField, DateTimeField
 
 app = Flask(__name__)
 api = Api(app)
+db = MongoEngine(app)
+
+app.config['MONGO_SETTINGS'] = {
+    'db': 'users',
+    'host': 'mongodb',
+    'port': '27017',
+    'username': '',
+    'password': ''
+}
+
+
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    first_name = StringField(required=True, unique=True)
+    last_name = StringField(required=True, unique=True)
+    email = StringField(required=True, unique=True)
+    birth_date = db.DateTimeField(required=True)
 
 
 class Users(Resource):
