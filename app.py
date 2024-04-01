ea07403d6d302ed7bcbb47db5d6e0e5097826e7c
Glaucia Lauton <glaucia.brito@maps.com.br>
2024-04-01 00:32:40 -0300

    fix: connection with mongodb and updating version

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
 from flask_mongoengine import MongoEngine
-from mongoengine.fields import StringField, DateTimeField
 
 app = Flask(__name__)
-api = Api(app)
-db = MongoEngine(app)
 
-app.config['MONGO_SETTINGS'] = {
+app.config['MONGODB_SETTINGS'] = {
     'db': 'users',
     'host': 'mongodb',
-    'port': '27017',
+    'port': 27017,
     'username': '',
     'password': ''
 }
 
+db = MongoEngine(app)
+api = Api(app)
+
 
 class UserModel(db.Document):
     cpf = db.StringField(required=True, unique=True)
-    first_name = StringField(required=True, unique=True)
-    last_name = StringField(required=True, unique=True)
-    email = StringField(required=True, unique=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    email = db.StringField(required=True)
     birth_date = db.DateTimeField(required=True)
 
 
 class Users(Resource):
     def get(self):
-        return {"user": "user 01"}
+        return jsonify(UserModel.objects())
 
 
 class User(Resource):
