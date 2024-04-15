67d8679c19219ceea2f12d4dd4ed510d1abb2825
Glaucia Brito <glaucia.brito@maps.com.br>
2024-04-15 00:21:04 -0300

    feat: insert data using reqparse

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
@@ -8,9 +8,36 @@ app.config['MONGODB_SETTINGS'] = {
     'db': 'users',
     'host': 'mongodb',
     'port': 27017,
-    'username': '',
-    'password': ''
+    'username': 'admin',
+    'password': 'admin'
 }
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('first_name',
+                          type=str,
+                          required=True,
+                          help="this field cannot be blank"
+                          )
+_user_parser.add_argument('last_name',
+                          type=str,
+                          required=True,
+                          help="this field cannot be blank"
+                          )
+_user_parser.add_argument('email',
+                          type=str,
+                          required=True,
+                          help="this field cannot be blank"
+                          )
+_user_parser.add_argument('cpf',
+                          type=str,
+                          required=True,
+                          help="this field cannot be blank"
+                          )
+_user_parser.add_argument('birth_date',
+                          type=str,
+                          required=True,
+                          help="this field cannot be blank"
+                          )
 
 db = MongoEngine(app)
 api = Api(app)
@@ -31,7 +58,8 @@ class Users(Resource):
 
 class User(Resource):
     def post(self):
-        return {"message": "test"}
+        data = _user_parser.parse_args()
+        UserModel(**data).save()
 
     def get(self, cpf):
         return {"message": "CPF"}
